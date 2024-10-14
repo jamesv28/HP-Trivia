a9b93371c85bcd02cc81b7e2c30a1e8c7d586fb0
James Volmert <james.volmert@gmail.com>
2024-10-14 17:53:32 -0600

    first commit

---
+++ HP-Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP-Trivia
+//
+//  Created by James Volmert on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HP-TriviaTests/HP_TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaTests.swift
+//  HP-TriviaTests
+//
+//  Created by James Volmert on 10/14/24.
+//
+
+import Testing
+@testable import HP_Trivia
+
+struct HP_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
